16957d71639998ce416f890ca2ccbf9427d3d527
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-08-17 22:13:22 +0300

    slider
    js

--- assets/js/slider-page-courses.js
+++ assets/js/slider-page-courses.js
@@ -3,23 +3,6 @@ $(document).ready(function () {
     arrows: true,
     dots: true,
     adaptiveHeight: true,
-    slideToShow: 1,
-    slideToScroll: 1,
-    speed: 800,
-    easing: 'linear',
-    infinite: true,
-    initialSlide: 0,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    draggable: true,
-    touchThreshold: 10,
-    touchMove: true,
-    waitForAnimate: true,
-  });
-  $('.slider-projects').slick({
-    arrows: true,
-    dots: false,
-    adaptiveHeight: false,
     slidesToShow: 2,
     slidesToScroll: 1,
     speed: 800,
@@ -27,22 +10,16 @@ $(document).ready(function () {
     infinite: true,
     initialSlide: 1,
     autoplay: true,
-    autoplaySpeed: 10000,
+    autoplaySpeed: 5000,
     draggable: true,
     touchThreshold: 10,
     touchMove: true,
     waitForAnimate: true,
-    centerMode: false,
-    variableWidth: false,
-    rows: 1,
-    // slidesPerRow: 1,
 
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          rows: 2,
-          slidesPerRow: 1,
           slidesToShow: 1,
           centerMode: false,
           variableWidth: false,
@@ -51,4 +28,39 @@ $(document).ready(function () {
       },
     ],
   });
+  // $('.slider-projects').slick({
+  //   arrows: true,
+  //   dots: false,
+  //   adaptiveHeight: false,
+  //   slidesToShow: 2,
+  //   slidesToScroll: 1,
+  //   speed: 800,
+  //   easing: 'linear',
+  //   infinite: true,
+  //   initialSlide: 1,
+  //   autoplay: true,
+  //   autoplaySpeed: 10000,
+  //   draggable: true,
+  //   touchThreshold: 10,
+  //   touchMove: true,
+  //   waitForAnimate: true,
+  //   centerMode: false,
+  //   variableWidth: false,
+  //   rows: 1,
+  //   // slidesPerRow: 1,
+
+  //   responsive: [
+  //     {
+  //       breakpoint: 1024,
+  //       settings: {
+  //         rows: 2,
+  //         slidesPerRow: 1,
+  //         slidesToShow: 1,
+  //         centerMode: false,
+  //         variableWidth: false,
+  //         draggable: false,
+  //       },
+  //     },
+  //   ],
+  // });
 });
