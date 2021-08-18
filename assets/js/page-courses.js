c0e9b3e823e53b16937d68425017168846f10018
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-08-18 13:26:54 +0300

    page
    js

--- assets/js/page-courses.js
+++ assets/js/page-courses.js
@@ -2,6 +2,6 @@ let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-$(function () {
-  $('*[placeholder="Контактный телефон*"]').mask('+7(000)000-00-00');
+$(document).ready(function () {
+  $('*[placeholder="Контактный телефон"]*').mask('+7(000)000-00-00');
 });
