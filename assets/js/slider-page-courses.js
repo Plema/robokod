9077c640ccde62c18e22fab4eb43789769c3cd71
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-08-19 09:41:32 +0300

    slider

--- assets/js/slider-page-courses.js
+++ assets/js/slider-page-courses.js
@@ -28,39 +28,48 @@ $(document).ready(function () {
       },
     ],
   });
-  // $('.slider-projects').slick({
-  //   arrows: true,
-  //   dots: false,
-  //   adaptiveHeight: false,
-  //   slidesToShow: 2,
-  //   slidesToScroll: 1,
-  //   speed: 800,
-  //   easing: 'linear',
-  //   infinite: true,
-  //   initialSlide: 1,
-  //   autoplay: true,
-  //   autoplaySpeed: 10000,
-  //   draggable: true,
-  //   touchThreshold: 10,
-  //   touchMove: true,
-  //   waitForAnimate: true,
-  //   centerMode: false,
-  //   variableWidth: false,
-  //   rows: 1,
-  //   // slidesPerRow: 1,
-
-  //   responsive: [
-  //     {
-  //       breakpoint: 1024,
-  //       settings: {
-  //         rows: 2,
-  //         slidesPerRow: 1,
-  //         slidesToShow: 1,
-  //         centerMode: false,
-  //         variableWidth: false,
-  //         draggable: false,
-  //       },
-  //     },
-  //   ],
-  // });
+  $('.slider-courses').slick({
+    arrows: true,
+    dots: true,
+    adaptiveHeight: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 800,
+    easing: 'linear',
+    infinite: true,
+    initialSlide: 1,
+    // autoplay: true,
+    autoplaySpeed: 10000,
+    draggable: true,
+    touchThreshold: 10,
+    touchMove: true,
+    waitForAnimate: true,
+    centerMode: false,
+    variableWidth: false,
+    rows: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          rows: 1,
+          slidesPerRow: 1,
+          slidesToShow: 2,
+          centerMode: false,
+          variableWidth: false,
+          draggable: false,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          rows: 1,
+          slidesPerRow: 1,
+          slidesToShow: 1,
+          centerMode: false,
+          variableWidth: false,
+          draggable: false,
+        },
+      },
+    ],
+  });
 });
