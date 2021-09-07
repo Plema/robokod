06db8ff267f57c2e097131fb7f72f209e83e7b20
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-09-07 18:12:56 +0300

    fix

--- assets/js/page-courses.js
+++ assets/js/page-courses.js
@@ -4,4 +4,19 @@ document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 $(document).ready(function () {
   $('*[placeholder="Контактный телефон *"]*').mask('+7(000)-000-00-00');
+
+  $('.rocket-anchor a').on('click', function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 700);
+  });
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 500) {
+      $('.rocket-anchor').addClass('show');
+    } else {
+      $('.rocket-anchor').removeClass('show');
+    }
+  });
 });
